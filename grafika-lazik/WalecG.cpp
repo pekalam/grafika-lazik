1a7fe89e16324c07941a39a5996f38f86246c477
Marek <marekbf3@gmail.com>
2019-03-28 13:53:44 +0100

    Walec - srodek

--- grafika-lazik/WalecG.cpp
+++ grafika-lazik/WalecG.cpp
@@ -7,10 +7,10 @@ void WalecG::drawObject()
 	GLfloat angle;
 	glColor3f(0.0, 0.0, 0.0);
 	glBegin(GL_TRIANGLE_FAN);
-	glVertex3d(0.0, 0.0, 0.0);
+	glVertex3d(0.0, 0.0, -_wys/2);
 	for (angle = 0.0; angle <= (2 * 3.14); angle += (3.14 / 8.0))
 	{
-		glVertex3d(_r*sin(angle), _r*cos(angle), 0.0);
+		glVertex3d(_r*sin(angle), _r*cos(angle), -_wys / 2);
 	}
 	glEnd();
 
@@ -20,16 +20,16 @@ void WalecG::drawObject()
 		for (angle = 0.0; angle <= (2 * 3.14); angle += (3.14 / 8.0))
 		{
 
-			glVertex3f(_r*sin(angle), _r*cos(angle), i*_wys / _divStep);
-			glVertex3f(_r*sin(angle), _r*cos(angle), (i + 1)*_wys / _divStep);
+			glVertex3f(_r*sin(angle), _r*cos(angle), i*-_wys/2 / _divStep + _wys/2);
+			glVertex3f(_r*sin(angle), _r*cos(angle), (i + 1)*-_wys/2 / _divStep);
 		}
 	}
 	glEnd();
 	glBegin(GL_TRIANGLE_FAN);
-	glVertex3f(0.0f, 0.0f, _wys);
+	glVertex3f(0.0f, 0.0f, _wys/2);
 	for (angle = 0.0; angle <= (2 * 3.14); angle += (3.14 / 8.0))
 	{
-		glVertex3f(_r*sin(angle), _r*cos(angle), _wys);
+		glVertex3f(_r*sin(angle), _r*cos(angle), _wys/2);
 	}
 	glEnd();
 
